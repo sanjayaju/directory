751c90624eb33bcf51b5f25f911200652af00b99
sanjayaju <sanjayaju789@gmail.com>
2024-08-22 22:08:07 -0700

    2commit

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BellIcon } from '@heroicons/react/24/outline';
+import img from '../assets/images/mypic.jpg'
 
 const Header = ({ adminName }) => {
   return (
@@ -9,7 +10,7 @@ const Header = ({ adminName }) => {
           <h1 className="text-2xl font-bold text-purple-600">PEOPLE.CO</h1>
           <div className="flex items-center space-x-4">
             <BellIcon className="w-6 h-6 text-gray-600 cursor-pointer" />
-            <div className="w-8 h-8 bg-gray-300 rounded-full"></div>
+            <img src={img} className="w-8 h-8 bg-gray-300 rounded-full"/>
             <span className="text-gray-700 font-medium">{adminName}</span>
           </div>
         </div>
